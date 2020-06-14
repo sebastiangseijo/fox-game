59cf1e556be8be31d8f55e430291d9360f041406
Sebastian <sebastiangseijo@gmail.com>
2020-06-15 01:55:31 +0200

    Add prettier rc configuration file

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  useTabs: false,
+  singleQuote: true,
+  arrowParens: 'always',
+  parser: 'babel-ts',
+};
